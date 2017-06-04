73d3f5391ccd240055198b24da8de6f04d04385c
Dragfreeze <Dragfreeze@gmail.com>
2017-06-04 14:28:58 +0530

    added stage-0 and removed server

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,23 @@
 var path = require('path')
 
 module.exports = {
-  entry: './app',
+  entry: './app/app.jsx',
   devtool: "cheap-eval-source-map",
   output: {
-    path: __dirname,
-    filename: './public/bundle.js'
-  },
-  resolve: {
-    modules: [__dirname, 'node_modules'],
-    alias: {
-
-    },
-    extensions: ['.js', '.jsx']
+    filename: 'bundle.js',
+    path: path.join(__dirname, "public"),
+    publicPath: 'public'
   },
   module: {
     loaders: [
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
       }
     ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    compress: true,
-    port: 3000
   }
 }
